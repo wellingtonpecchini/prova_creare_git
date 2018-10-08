3eea20a1c7e3fe17adcf96dcd5fed7f166454087
wellington <wellington.pecchini@bitrock.it>
2018-10-08 17:34:27 +0200

    preso i primi dati

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ import {html, render} from './node_modules/lit-html/lit-html.js';
 
 
 const root = document.querySelector("#root");
+
+
+
+
 // Define a template
-const myTemplate = (name) => html`<p>Hello ${name}</p>`;
+const myTemplate = (data) => html`<p>Hello ${data.name.first} ${data.name.last}</p>`;
+
 
 // Render the template to the document
-render(myTemplate('World'), root);
+// render(myTemplate('World'), root);
+
+
+
+fetch("https://randomuser.me/api/")
+.then(r => r.json())
+// .then(r => console.log(JSON.stringify(r)));
+.then(r => r.results[0])
+.then(r => render(myTemplate(r), root));
+ // prendo il (myTeplate) per prendere la mia struttura, mentre il root è dove vado buttarlo dentro
